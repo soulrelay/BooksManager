c2ed9a3bc8302953b4a25aee0dfbe95002b4e670
feifei003603 <854535609@qq.com>
2013-07-23 13:17:36 +0800

    sssss

--- goods/testing.php
+++ goods/testing.php
@@ -1,1 +1,19 @@
-phddddddddddddp
+class Album {
+		function index(){
+			$album=D("album");
+			$this->mess('提示：删除相册时只能删除空相册，如果相册下有子相册或有图片，请先删除子分类和图片. ');
+			$this->assign("list", $album->parsetree());
+			$this->display();
+		}
+
+		function add(){
+			$album=D("album");
+			$this->mess('提示: 带<span class="red_font">*</span>的项目为必填信息. ');
+			$this->assign("select", $album->formselect());
+			$this->display();
+		}
+phdddddddddf
+
+
+
+gggggggggggggggggggggggggggg
